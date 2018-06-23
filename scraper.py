c728e28c9ac0e827965a5395f10b0742dffb481b
Sahith Nallapareddy <sahith.reddy@gmail.com>
2018-06-23 12:26:12 -0700

    updated scraper

--- scraper.py
+++ scraper.py
@@ -1,9 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
 import datetime
 import sys
 import getpass
 
+chrome_options = Options()
+#chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=1920x1080")
+prefs = {
+    "profile.default_content_setting_values.plugins": 1,
+    "profile.content_settings.plugin_whitelist.adobe-flash-player": 1,
+    "profile.content_settings.exceptions.plugins.*,*.per_resource.adobe-flash-player": 1,
+    "PluginsAllowedForUrls": "http://seller.samsungapps.com"
+}
+chrome_options.add_experimental_option("prefs",prefs)
 def get_totals_for_days(date1, date2, browser):
 	timedelta = date2 - date1
 	if (timedelta.days > 31):
@@ -36,15 +47,16 @@ def get_totals_for_days(date1, date2, browser):
 	return totals
 
 
-browser = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true', '--ssl-protocol=TLSv1'])
+browser = webdriver.Chrome(chrome_options=chrome_options)
 browser.set_window_size(1120, 550)
-url = 'http://seller.samsungapps.com/login/signIn.as?returnURL=%2fmain%2fsellerMain.as&ssoCheck=fail'
+url = 'https://seller.samsungapps.com/login/signIn.as?returnURL=%2fmain%2fsellerMain.as&ssoCheck=fail'
 browser.get(url)
+browser.find_element_by_class_name("btnGateLogin").click()
 username = input("Username: ")
 password = getpass.getpass("Password: ")
-browser.find_element_by_id("emailID").send_keys(username)
-browser.find_element_by_id("password").send_keys(password)
-browser.find_element_by_class_name("btnNew01").click()
+browser.find_element_by_id("iptLgnPlnID").send_keys(username)
+browser.find_element_by_id("iptLgnPlnPD").send_keys(password)
+browser.find_element_by_id("signInButton").click()
 browser.get("http://seller.samsungapps.com/statistics/statisticsDownloadsSales.as")
 totals = ""
 startDateString = input("Start Date (mm/dd/yyyy): ")
